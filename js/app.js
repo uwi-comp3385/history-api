008195b20cc5c6673c0f575fde625eac0435ffd5
Yannick Lyn Fatt <yannick.lynfatt@gmail.com>
2018-02-21 22:55:35 -0500

    Fix an issue with back button not loading proper page and updating url accordingly

--- js/app.js
+++ js/app.js
@@ -8,15 +8,22 @@ window.onload = function() {
       let url = e.target.href;
       let linkTitle = e.target.textContent;
       getContent(url, linkTitle);
+      history.pushState(null, linkTitle, url);
+      // Not all browsers support the 2nd parameter for the title of the history
+      // entry, however, you can still put something there.
     });
   });
+  // console.log(location.pathname);
+  getContent(location.pathname)
 };
 
-var getContent = function (url, title) {
-  $('#main').load(url + ' #main');
-  history.pushState(null, title, url);
-  // Not all browsers support the 2nd parameter for the title of the history
-  // entry, however, you can still put something there.
+var getContent = function (url) {
+  let urlToLoad = (url === '/') ? 'index' : url;
+  // The load() function is a part of the AJAX functions
+  // bundled with jQuery and is short hand for making
+  // an AJAX call to page and getting the content in an element
+  // in this case an element with an id of 'main'.
+  $('#main').load(urlToLoad + '.html #main');
 };
 
 // Adding popstate event listener to handle browser back button
